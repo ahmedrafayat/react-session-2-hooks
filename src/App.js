247430d4b22ba67b19f0ad3e40c9684049c907d6
ahmedrafayat <ahmed.rafayat97@gmail.com>
2022-09-13 11:34:48 +0600

    UseReducerPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Link, Route, Routes } from "react-router-dom";
 import UseEffectPage from "./pages/UseEffectPage";
+import UseReducerPage from "./pages/UseReducerPage";
 import UseStatePage from "./pages/UseStatePage";
 
 function App() {
@@ -15,11 +16,15 @@ function App() {
           <li>
             <Link to={'/useeffect'}>Use Effect</Link>
           </li>
+          <li>
+            <Link to={'/usereducer'}>Use Reducer</Link>
+          </li>
         </ol>
       </div>
       <Routes>
         <Route path={'/usestate'} element={<UseStatePage />} />
         <Route path={'/useeffect'} element={<UseEffectPage />} />
+        <Route path={'/usereducer'} element={<UseReducerPage />} />
       </Routes>
     </div>
   );
